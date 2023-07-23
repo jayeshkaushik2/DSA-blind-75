88c466306d868c63a7656d207940499f9bdc1260
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-07-23 22:54:24 +0530

    update: sliding window hard question solution.

--- Sliding window/MinimumWindowSubstring.py
+++ Sliding window/MinimumWindowSubstring.py
@@ -2,6 +2,42 @@ import collections
 
 
 def minWindow(s: str, t: str) -> str:
+    # time comp O(N) space comp O(N + M)
+    if t == "":
+        return ""
+    countT, window = {}, {}
+    for c in t:
+        countT[c] = 1 + countT.get(c, 0)
+    l = 0
+    res = [-1, -1]
+    resLen = float("inf")
+    have, need = 0, len(countT)
+
+    for r in range(len(s)):
+        c = s[r]
+        # Storing each ch from c to window
+        window[c] = 1 + window.get(c, 0)
+        # checking if c present in t and updating the have value by 1
+        if c in countT and window[c] == countT[c]:
+            have += 1
+
+        # decrease the window size till have == need and update the resLen
+        while have == need:
+            if (r - l + 1) < resLen:
+                resLen = r - l + 1
+                res = [l, r]
+
+            window[s[l]] -= 1
+
+            # check if window[c] < countT[c] if does means we have to decrement the have by 1
+            if s[l] in countT and countT[s[l]] > window[s[l]]:
+                have -= 1
+            l += 1
+
+    l, r = res
+
+    return s[l : r + 1] if resLen != float("inf") else ""
+
     def form_substring(ss):
         hm = collections.Counter(t)
         # when hm's values sum is equal to 0 thats when we know we found the sub string
